f6cf6026bb10349ef719621dee853edd50b9561b
SRJavier97 <sr_javier97@hotmail.com>
2018-04-30 13:19:43 -0500

    Creacion_de_Rama_Javier

---
+++ app/src/main/java/mx/unam/sergioguerrero/proyecto_aplicacion/AcercaDeDialogo.java
@@ -0,0 +1,13 @@
+package mx.unam.sergioguerrero.proyecto_aplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcercaDeDialogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de_dialogo);
+    }
+}
