a036440bf08b185fdff96b4caad5ec42aeb6f026
SergilGG <sergil51@hotmail.com>
2018-05-29 23:04:39 -0500

    Correcion layouts

--- app/src/main/java/mx/unam/sergioguerrero/proyecto_aplicacion/ConsultarListaListView.java
+++ app/src/main/java/mx/unam/sergioguerrero/proyecto_aplicacion/ConsultarListaListView.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.R;
 
 import java.util.ArrayList;
 
